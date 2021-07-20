45b3be8cc44ef751bb89a8491c5989f284b939ed
Damian <pokemonmew34567@gmail.com>
2021-07-20 03:24:44 -0700

    js trouble

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-let searchbar = document.getElementsByClassName("main-search-bar");
+let inputButton = document.getElementById("button-id");
+let inputText = document.getElementById("inputId");
+let inputContainer = document.getElementById("text-div");
+let title = document.getElementById("title");
+title.innerHTML = "aswe";
+
+const textChange = () => {
+  title.innerHTML = "eeeeeeeeeeee";
+}
+inputButton.addEventListener('click', textChange);
