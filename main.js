7cc12d4f377241309007e95a00ba2691f440ada9
Damian <pokemonmew34567@gmail.com>
2021-07-20 23:30:11 -0700

    d

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-let inputButton = document.getElementById("button-id");
 let inputText = document.getElementById("inputId");
-let inputContainer = document.getElementById("text-div");
 let title = document.getElementById("title");
-title.innerHTML = "aswe";
 
-const textChange = () => {
-  title.innerHTML = "eeeeeeeeeeee";
-}
-inputButton.addEventListener('click', textChange);
