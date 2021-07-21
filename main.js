b2d58920cdae9794f489249533cb3e5591bbdc04
Damian <pokemonmew34567@gmail.com>
2021-07-21 04:00:21 -0700

    todo list has begun

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let inputText = document.getElementById("inputId");
-let title = document.getElementById("title");
+let inputButton = document.getElementById("button-id");
+let inputContainer = document.getElementById("todo-card-div");
 
+
+inputButton.addEventListener('click', function() {
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "hi";
+    inputContainer.appendChild(paragraph);
+});
